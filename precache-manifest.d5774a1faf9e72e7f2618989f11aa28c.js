72cef2c266294febe005124560537779ada53a66
Ankit Ahir <ahir.ankit93@gmail.com>
2020-06-26 15:42:49 +0530

    Updates

---
+++ precache-manifest.d5774a1faf9e72e7f2618989f11aa28c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2037e610c00ee7ac76c35587d4cb8f",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "3890454cd14b62709438",
+    "url": "/portfolio-app/static/css/2.eafa209c.chunk.css"
+  },
+  {
+    "revision": "ce2d63107a49048058fb",
+    "url": "/portfolio-app/static/css/main.8575169f.chunk.css"
+  },
+  {
+    "revision": "3890454cd14b62709438",
+    "url": "/portfolio-app/static/js/2.d92a9959.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/portfolio-app/static/js/2.d92a9959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2d63107a49048058fb",
+    "url": "/portfolio-app/static/js/main.8f3a2552.chunk.js"
+  },
+  {
+    "revision": "9273fb07e8f782d2211c",
+    "url": "/portfolio-app/static/js/runtime-main.9d41a791.js"
+  },
+  {
+    "revision": "d716f7f68bc61be34188e74fa2865c71",
+    "url": "/portfolio-app/static/media/ankit.d716f7f6.JPG"
+  },
+  {
+    "revision": "558e162ff3963ceeb3e046b108e0e653",
+    "url": "/portfolio-app/static/media/bg1.558e162f.jpg"
+  },
+  {
+    "revision": "1f802836613e49af2fdd39de3139fd52",
+    "url": "/portfolio-app/static/media/painting-palette.1f802836.png"
+  }
+]);
